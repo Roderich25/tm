fc5c630c69ebbc533c2228902352b0cf2ac8be73
Roderich25 <rodrigoavilasolis@gmail.com>
2019-05-15 17:16:27 -0500

    supervised learning with svm

--- demo.py
+++ demo.py
@@ -1,7 +1,58 @@
-#nltk.download()
-from nltk.book import *
+import pandas as pd
+import numpy as np
+from nltk.tokenize import word_tokenize
+from nltk import pos_tag
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.preprocessing import LabelEncoder
+from collections import defaultdict
+from nltk.corpus import wordnet as wn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn import model_selection, naive_bayes, svm
+from sklearn.metrics import accuracy_score
 
-def lexical_diversity(text):
-    return len(set(text))/len(text)
+np.random.seed(2012)
+Corpus = pd.read_csv(r"C:\Users\ravialas\Desktop\corpus.csv", encoding='latin-1')
+# Remove blank rows if any.
+Corpus['TEXT'].dropna(inplace=True)
+# Change all the text to lower case.
+Corpus['TEXT'] = [entry.lower() for entry in Corpus['TEXT']]
+# Tokenization
+Corpus['TEXT'] = [word_tokenize(entry) for entry in Corpus['TEXT']]
 
-lexical_diversity(text3)
+# Remove Stop words, Non-Numeric and Word Stemming.
+tag_map = defaultdict(lambda: wn.NOUN)
+tag_map['J'] = wn.ADJ
+tag_map['V'] = wn.VERB
+tag_map['R'] = wn.ADV
+for index, entry in enumerate(Corpus['TEXT']):
+    Final_words = []
+    word_Lemmatized = WordNetLemmatizer()
+    for word, tag in pos_tag(entry):
+        if word not in stopwords.words('english') and word.isalpha():
+            word_Final = word_Lemmatized.lemmatize(word, tag_map[tag[0]])
+            Final_words.append(word_Final)
+    Corpus.loc[index, 'text_final'] = str(Final_words)
+
+Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(Corpus['text_final'],Corpus['LABEL'], test_size=0.2)
+
+Encoder = LabelEncoder()
+Train_Y = Encoder.fit_transform(Train_Y)
+Test_Y = Encoder.fit_transform(Test_Y)
+
+Tfidf_vect = TfidfVectorizer(max_features=5000)
+Tfidf_vect.fit(Corpus['text_final'])
+Train_X_Tfidf = Tfidf_vect.transform(Train_X)
+Test_X_Tfidf = Tfidf_vect.transform(Test_X)
+
+print(Tfidf_vect.vocabulary_)
+print(Train_X_Tfidf)
+
+# Classifier - Algorithm - SVM
+# fit the training dataset on the classifier
+SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+SVM.fit(Train_X_Tfidf,Train_Y)
+# predict the labels on validation dataset
+predictions_SVM = SVM.predict(Test_X_Tfidf)
+# A
+print("SVM Accuracy Score -> ", accuracy_score(predictions_SVM, Test_Y)*100)
