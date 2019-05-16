1c11a29485b0f54d985fe6faa003c0f04e279f4e
Roderich25 <rodrigoavilasolis@gmail.com>
2019-05-16 17:32:55 -0500

    new file test added to repository

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import re
 from nltk.tokenize import word_tokenize
 from nltk import pos_tag
 from nltk.corpus import stopwords
@@ -13,14 +14,17 @@ from sklearn.metrics import accuracy_score
 
 np.random.seed(2012)
 Corpus = pd.read_csv(r"C:\Users\ravialas\Desktop\corpus.csv", encoding='latin-1')
-# Remove blank rows if any.
-Corpus['TEXT'].dropna(inplace=True)
-# Change all the text to lower case.
+Corpus = Corpus[['LABEL', 'TEXT']]
+Corpus_filter = dict(Corpus.groupby('LABEL').TEXT.count() > 4)
+Corpus['Bool'] = Corpus['LABEL'].map(Corpus_filter)
+Corpus = Corpus[Corpus['Bool'] == True]
 Corpus['TEXT'] = [entry.lower() for entry in Corpus['TEXT']]
-# Tokenization
+Corpus['TEXT'] = [re.sub(r'[^\w\s]', '', w) for w in Corpus['TEXT']]  # if w is w.isalnum()]
 Corpus['TEXT'] = [word_tokenize(entry) for entry in Corpus['TEXT']]
 
-# Remove Stop words, Non-Numeric and Word Stemming.
+
+
+# Remove Stop words, and Word Stemming.
 tag_map = defaultdict(lambda: wn.NOUN)
 tag_map['J'] = wn.ADJ
 tag_map['V'] = wn.VERB
@@ -29,12 +33,13 @@ for index, entry in enumerate(Corpus['TEXT']):
     Final_words = []
     word_Lemmatized = WordNetLemmatizer()
     for word, tag in pos_tag(entry):
-        if word not in stopwords.words('english') and word.isalpha():
+        if word not in stopwords.words('english'):
             word_Final = word_Lemmatized.lemmatize(word, tag_map[tag[0]])
             Final_words.append(word_Final)
     Corpus.loc[index, 'text_final'] = str(Final_words)
 
-Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(Corpus['text_final'], Corpus['LABEL'], test_size=0.2)
+Train_X, Test_X, Train_Y, Test_Y = model_selection.train_test_split(Corpus['text_final'], Corpus['LABEL'],
+                                                                    test_size=0.3)
 
 Encoder = LabelEncoder()
 Train_Y = Encoder.fit_transform(Train_Y)
@@ -51,8 +56,16 @@ print(Train_X_Tfidf)
 # Classifier - Algorithm - SVM
 # fit the training dataset on the classifier
 SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
-SVM.fit(Train_X_Tfidf,Train_Y)
+SVM.fit(Train_X_Tfidf, Train_Y)
 # predict the labels on validation dataset
 predictions_SVM = SVM.predict(Test_X_Tfidf)
 # A
-print("SVM Accuracy Score -> ", accuracy_score(predictions_SVM, Test_Y)*100)
+print("SVM Accuracy Score -> ", accuracy_score(predictions_SVM, Test_Y) * 100)
+
+# fit the training dataset on the NB classifier
+Naive = naive_bayes.MultinomialNB()
+Naive.fit(Train_X_Tfidf, Train_Y)
+# predict the labels on validation dataset
+predictions_NB = Naive.predict(Test_X_Tfidf)
+# Use accuracy_score function to get the accuracy
+print("Naive Bayes Accuracy Score -> ", accuracy_score(predictions_NB, Test_Y) * 100)
---
+++ test.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import re
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+df = pd.read_csv(r"C:\Users\ravialas\Desktop\corpus.csv", encoding='latin-1')
+df = df[['LABEL', 'TEXT']]
+df_filter = dict(df.groupby('LABEL').TEXT.count()>32)
+df['Bool'] = df['LABEL'].map(df_filter)
+df = df[df['Bool'] == True]
+df['TEXT'] = [re.sub(r'[^A-Za-z0-9 ]+', ' ', w) for w in df['TEXT']]
+df['TEXT'] = [re.sub(' +', ' ', w).lower() for w in df['TEXT']]
+#df['TEXT'] = [word_tokenize(entry) for entry in df['TEXT']]
+#df['TEXT'] = [word for word in df['TEXT'] if word not in stopwords.words('english')]
+
+df['category_id'] = df['LABEL'].factorize()[0]
+category_id_df = df[['LABEL', 'category_id']].drop_duplicates().sort_values('category_id')
+category_to_id = dict(category_id_df.values)
+id_to_category = dict(category_id_df[['category_id', 'LABEL']].values)
+
+tfidf = TfidfVectorizer(sublinear_tf=True, max_df=1.0, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words='english', max_features=4000)
+features = tfidf.fit_transform(df.TEXT).toarray()
+labels = df.category_id
+features.shape
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score
+from sklearn import svm
+X_train, X_test, y_train, y_test = train_test_split(df['TEXT'], df['LABEL'], random_state = 0)
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(X_train)
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+
+clf = MultinomialNB().fit(X_train_tfidf, y_train)
+predictions_nb = clf.predict(count_vect.transform(X_test))
+accuracy_score(predictions_nb, y_test) * 100
+
+SVM = svm.SVC(C=1.0, kernel='linear', degree=3, gamma='auto')
+SVM.fit(X_train_tfidf, y_train)
+predictions_svm = SVM.predict(count_vect.transform(X_test))
+accuracy_score(predictions_svm, y_test) * 100
